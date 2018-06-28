3e51de52014c701f1603c3866a3e4a3f6452a53f
Unknown <hoyounn@naver.com>
2018-06-28 16:32:43 +0900

    frontcontroller *.bo

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("/FrontController")
+@WebServlet("*.bo")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
