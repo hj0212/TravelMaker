e7792e37ad87cb957cf7f83317c551af5404fe39
Unknown <hoyounn@naver.com>
2018-06-28 15:55:42 +0900

    ReviewDTO ReviewDAO 생성
    reviewboard list dao 생성

--- src/dao/ReviewDAO.java
+++ src/dao/ReviewDAO.java
@@ -1,5 +1,35 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import DBUtils.DBConnection;
+import dto.ReviewDTO;
+
 public class ReviewDAO {
 
+	public List<ReviewDTO> getAllArticles (int review_seq) throws Exception{
+		Connection con = DBConnection.getConnection();
+		String sql = "select * from reviewboard where review_seq=?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setInt(1, review_seq);
+		ResultSet rs = pstmt.executeQuery();
+		List<ReviewDTO> result = new ArrayList<>();
+		while(rs.next()) {
+			ReviewDTO rdto = new ReviewDTO();
+			rdto.setReview_seq(rs.getInt("review_seq"));
+			rdto.setReview_title(rs.getString("review_title"));
+			rdto.setReview_writer("review_writer");
+			rdto.setReview_writedate(rs.getString("review_writedate"));
+			rdto.setReview_viewcount(rs.getInt("review_viewcount"));
+			result.add(rdto);
+		}
+		rs.close();
+		pstmt.close();
+		con.close();
+		return result;
+	}
 }
