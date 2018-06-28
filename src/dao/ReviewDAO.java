dd710609e008b18b2f069621b6f6c9854c00813e
Unknown <hoyounn@naver.com>
2018-06-28 16:18:32 +0900

    getArticle

--- src/dao/ReviewDAO.java
+++ src/dao/ReviewDAO.java
@@ -32,4 +32,50 @@ public class ReviewDAO {
 		con.close();
 		return result;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public ReviewDTO getReviewArticle(int review_seq) throws Exception{
+		Connection con = DBConnection.getConnection();
+		String sql = "select * from reviewboard where review_seq = ?";
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setInt(1, review_seq);
+		ResultSet rs = pstmt.executeQuery();
+		ReviewDTO rdto = new ReviewDTO();
+		if(rs.next()) {
+			rdto.setReview_seq(rs.getInt("reviw_seq"));
+			rdto.setReview_title(rs.getString("review_title"));
+			rdto.setReview_contents(rs.getString("reviw_contents"));
+			rdto.setReview_writer("reviewWriter");
+			rdto.setReview_writedate(rs.getString("review_writedate"));
+			rdto.setReview_viewcount(rs.getInt("review_viewcount"));
+		}
+		rs.close();
+		pstmt.close();
+		con.close();
+		return rdto;
+		
+	}
 }
